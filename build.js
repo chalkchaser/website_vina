718d7641d9a8276e065e5794c7ec4cb2df7cfbb8
vintieu <kebintieu@gmail.com>
2020-10-10 18:38:13 +0200

    added flexbox to display felines

--- build.js
+++ build.js
@@ -3,56 +3,60 @@
     var products
     readJson()
 
-
-
-
     function readJson(){
-    $.ajax({
-        url: "produktdata.json",
-        //force to handle it as text
-        dataType: "text",
-        success: function(data) {
+        $.ajax({
+            url: "produktdata.json",
+            //force to handle it as text
+            dataType: "text",
+            success: function(data) {
 
-            jsondata = $.parseJSON(data);
-            //now json variable contains data in json format
-            //Now you can get your data like json.name, json.object
-            products = jsondata["Produkte"]
-            groups = jsondata["Gruppen"]
-            build(products)
-            generateHyperlinks(groups)
-        }
-    });
+                jsondata = $.parseJSON(data);
+                //now json variable contains data in json format
+                //Now you can get your data like json.name, json.object
+                products = jsondata["Produkte"]
+                groups = jsondata["Gruppen"]
+                build(products)
+                generateHyperlinks(groups)
+            }
+        });
 
-}
+    }
 
     function generateHyperlinks(data){
     var obj = data
-        jQuery.each(data, function (key1, value){
+        jQuery.each(data, function (key1, value){ //loop over groups
             var divelement = document.createElement('div')
             $(divelement).attr("id", key1)
             document.getElementById("hyperlinks").append(divelement)
-            var element = document.createElement('button');
+            var element = document.createElement('button');//adds button for groups
 
-            $(element).attr("onclick", "clicked("+ JSON.stringify(key1)+ ")")
+            $(element).attr("onclick", "clicked("+ JSON.stringify(key1)+ ")") //add click functionality
             element.innerHTML = key1
 
             document.getElementById(key1).append(element)
 
 
             jQuery.each(value, function (key2, value2){//sublinks
-                var element2 = document.createElement('button')
-                $(element2).attr("class" ,"button "+ key1)
-                //$(element2).attr("display","none")//hides subgroups
-                $(element2).attr("onclick", "clicked("+ JSON.stringify(value2)+ ")")
-                element2.innerHTML = value2
+                jQuery.each(value2, function (key3, value3){
 
-                document.getElementById(key1).append(element2)
-                $(element2).hide()
+                    var element2 = document.createElement('button')
+                    $(element2).attr("class" ,"button "+ key1)
+                    //$(element2).attr("display","none")//hides subgroups
+                    $(element2).attr("onclick", "clicked("+ JSON.stringify(value3)+ ")")
+                    element2.innerHTML = value3
+
+                    document.getElementById(key1).append(element2)
+                    $(element2).hide()
+
+
+                })
+
+
+                })
+
             })
 
-        })
-
-}
+    }
 
     function clicked(key) {
        // alert((key))
@@ -67,37 +71,48 @@
     var group = g1
     document.getElementById("itemdisplay").innerText = ""
     jQuery.each(data, function(key1,value){
-    items = document.getElementById("itemdisplay")
+        items = document.getElementById("itemdisplay")
+        single_item = document.createElement("li")
+        single_item.className = "flex_item"
+        if(value.G1 === group ||value.G2 === group || g1 === undefined) {
+            var nummer = document.createElement('div');
+            nummer.innerHTML += "nr: " + key1;
 
-    if(value.G1 === group ||value.G2 === group || g1 === undefined) {
-        var nummer = document.createElement('div');
-        nummer.innerHTML += "nr: " + key1;
+            var name = document.createElement('div');
+            name.innerHTML += value.Name;
 
-        var name = document.createElement('div');
-        name.innerHTML += value.Name;
+            var preis = document.createElement('div');
+            preis.innerHTML += "Preis: " + value.Preis + "€";
 
-        var preis = document.createElement('div');
-        preis.innerHTML += "Preis: " + value.Preis + "€";
+            var ve = document.createElement('div');
+            ve.innerHTML += "VE: " + value.VE;
 
-        var ve = document.createElement('div');
-        ve.innerHTML += "VE: " + value.VE;
+            var beschreibung = document.createElement('div');
+            beschreibung.innerHTML += " " + value.Beschreibung;
 
-        var beschreibung = document.createElement('div');
-        beschreibung.innerHTML += " " + value.Beschreibung;
+            single_item.appendChild(nummer)
+            single_item.appendChild(name)
+            single_item.appendChild(preis)
 
-        items.appendChild(nummer)
-        items.appendChild(name)
-        items.appendChild(preis)
+            single_item.appendChild(ve)
+            single_item.appendChild(beschreibung)
 
-        items.appendChild(ve)
-        items.appendChild(beschreibung)
-        var newPicture = document.createElement("img")
+            var productImageDivider = document.createElement("div")
+            productImageDivider.className = "productImages"
+            var newPicture = document.createElement("img")
 
-        $(newPicture).attr("src", "images/" + key1 + ".jpg")
-        //$("#img"+key1).wrap("<a href='images/0000.jpg'></a>");
+            $(newPicture).attr("src", "images/" + key1 + ".jpg")
+            //$("#img"+key1).wrap("<a href='images/0000.jpg'></a>");
 
-    items.appendChild(newPicture)
-}
+            productImageDivider.appendChild(newPicture)//adds a container for images resizing
+
+         single_item.appendChild(productImageDivider)
+        items.appendChild(single_item)
+        if(g1 !== undefined) {
+            document.getElementById("currentlocation").innerHTML = g1
+        }else {document.getElementById("currentlocation").innerHTML = "Alle"}
+
+        }
 
 
 });
